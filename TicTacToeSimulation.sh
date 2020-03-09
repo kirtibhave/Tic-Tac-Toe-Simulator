b8c0dc6e0f2d7429a8bf23ade0977449aa7780fd
kirtibhave <kirtibhave1000@gmail.com>
2020-03-09 17:53:06 +0530

    ADDED:Function to start fresh by resetting the board

--- TicTacToeSimulation.sh
+++ TicTacToeSimulation.sh
@@ -1,2 +1,22 @@
+#!/bin/bash
 echo "********************Welcome to TicTacToe Simulation Problem********************"
 
+#CONSTATNTS
+ROW=$1
+COLUMN=$2
+
+declare -a board
+
+#function is used to start fresh by resetting the board
+function resetBoard(){
+for (( i=1;i<=$ROW;i++ ))
+do
+	for (( j=1;j<=$COLUMN;j++ ))
+	do
+		board[$i,$j]="_"
+	done
+	 echo ${board[@]}
+done
+}
+
+resetBoard
