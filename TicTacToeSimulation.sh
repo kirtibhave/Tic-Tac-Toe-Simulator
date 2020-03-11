2ce9bd1eb0192701c9f9a2dab16f031e4a349a5b
kirtibhave <kirtibhave1000@gmail.com>
2020-03-11 15:45:51 +0530

    ADDED:Function to check block posssibility of computer

--- TicTacToeSimulation.sh
+++ TicTacToeSimulation.sh
@@ -86,7 +86,6 @@ read -p "enter row & column number to insert value" rowNumber columnNumber
 	then
 		board[$rowNumber,$columnNumber]=$player
 		boardConditions
-		winPossiblityOfComputer
 		computerInput
 	else
 		echo "OOPS! Position is occupied"
@@ -96,21 +95,25 @@ read -p "enter row & column number to insert value" rowNumber columnNumber
 
 #to take input from computer and to choose valid cells
 function computerInput(){
-winPossiblityOfComputer
+computersWinPossiblity
 if [[ $flag -eq 0 ]]
 then
-	rowNumber2=$(($RANDOM%3+1))
-	columnNumber2=$(($RANDOM%3+1))
-	echo "Computer will be played first"
-	if [[ ${board[$rowNumber2,$columnNumber2]} == "-" ]]
+	computersBlockPossibility
+	if [[ $flag -eq 0 ]]
 	then
-		board[$rowNumber2,$columnNumber2]=$computer
-		boardConditions
-		userInput
-	else
-		echo "OOPS! Position is occupied"
-		computerInput
-   fi
+		rowNumber2=$(($RANDOM%3+1))
+		columnNumber2=$(($RANDOM%3+1))
+		echo "Computer will be played"
+			if [[ ${board[$rowNumber2,$columnNumber2]} == "-" ]]
+			then
+				board[$rowNumber2,$columnNumber2]=$computer
+				boardConditions
+				userInput
+			else
+				echo "OOPS! Position is occupied"
+				computerInput
+			fi
+	fi
 fi
 }
 
@@ -158,7 +161,7 @@ done
 }
 
 #function is used to check win possibility of computer
-function winPossiblityOfComputer(){
+function computersWinPossiblity(){
 #for horizontal
 flag=0
 for (( i=1;i<=$ROW;i++ ))
@@ -217,13 +220,82 @@ do
 			board[$(($i+2)),$j]=$computer
 			flag=1
 		fi
+
+		if [ $flag -eq 1 ]
+		then
+			displayBoard
+			winConditions
+		fi
+	done
+done
+}
+
+# to check block condition for player
+function computersBlockPossibility(){
+#for horizontal
+for (( i=1;i<=$ROW;i++ ))
+do
+   for (( j=1;j<=$COLUMN;j++ ))
+   do
+	if [[ ${board[$i,$j]} == "-" && ${board[$i,$(($j+1))]} == $player && ${board[$i,$(($j+2))]} == $player ]]
+	then
+		board[$i,$j]=$computer
+		flag=1
+	elif [[ ${board[$i,$j]} == $player && ${board[$i,$(($j+1))]} == "-" && ${board[$i,$(($j+2))]} == $player ]]
+	then
+		board[$i,$(($j+1))]=$computer
+		flag=1
+	elif [[ ${board[$i,$j]} == $player && ${board[$i,$(($j+1))]} == $player && ${board[$i,$(($j+2))]} == "-" ]]
+	then
+		board[$i,$(($j+2))]=$computer
+		flag=1
+#for Vertical
+	elif [[ ${board[$j,$i]} == "-" && ${board[$(($j+1)),$i]} == $player && ${board[$(($j+2)),$i]} == $player ]]
+	then
+		board[$j,$i]=$computer
+		flag=1
+	elif [[ ${board[$j,$i]} == $player && ${board[$(($j+1)),$i]} == "-" && ${board[$(($j+2)),$i]} == $player ]]
+	then
+		board[$(($j+1)),$i]=$computer
+		flag=1
+	elif [[ ${board[$j,$i]} == $player && ${board[$(($j+1)),$i]} == $player && ${board[$(($j+2)),$i]} == "-" ]]
+	then
+		board[$(($j+2)),$i]=$computer
+		flag=1
+#1st diagonal
+	elif [[ ${board[$i,$j]} == "-" && ${board[$(($i+1)),$(($j+1))]} == $player && ${board[$(($i+2)),$(($j+2))]} == $player ]]
+	then
+		board[$i,$j]=$computer
+		flag=1
+	elif [[ ${board[$i,$j]} == $player && ${board[$(($i+1)),$(($j+1))]} == "-" && ${board[$(($i+2)),$(($j+2))]} == $player ]]
+	then
+		board[$(($i+1)),$(($j+1))]=$computer
+		flag=1
+	elif [[ ${board[$i,$j]} == $player && ${board[$(($i+1)),$(($j+1))]} == $player && ${board[$(($i+2)),$(($j+2))]} == "-" ]]
+	then
+		board[$(($i+2)),$(($j+2))]=$computer
+		flag=1
+#2nd diagonal
+	elif [[ ${board[$i,$(($j+2))]} == "-" && ${board[$(($i+1)),$(($j+1))]} == $player && ${board[$(($i+2)),$j]} == $player ]]
+	then
+		board[$i,$(($j+2))]=$computer
+		flag=1
+	elif [[ ${board[$i,$(($j+2))]} == $player && ${board[$(($i+1)),$(($j+1))]} == "-" && ${board[$(($i+2)),$j]} == $player ]]
+	then
+		board[$(($i+1)),$(($j+1))]=$computer
+		flag=1
+	elif [[ ${board[$i,$(($j+2))]} == $player && ${board[$(($i+1)),$(($j+1))]} == $player && ${board[$(($i+2)),$j]} == "-" ]]
+	then
+		board[$(($i+2)),$j]=$computer
+		flag=1
+	fi
 	done
 done
 
 if [ $flag -eq 1 ]
 then
+	((count++))
 	displayBoard
-	winConditions
 fi
 }
 
